a4b62ededf37f4e2dc8343de71652867d9049527
tanuja <tanujatiwari04@gmail.com>
2022-09-08 01:05:41 +0530

    fix export app

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-import express from 'express'
+import express, { Application } from 'express'
 
 import publicRoutes from './routes/index'
 import errorHandler from './middlewares/errorHandler'
@@ -9,30 +9,31 @@ import os from 'os';
 import cluster from 'cluster';
 import job from './cron/index'
 
-import mockJob from './cron/mock'
-
 const totalCpus = os.cpus().length
 
-// if (cluster.isMaster) {
-//     console.log(`Number of CPUs is ${totalCpus}`);
-//     console.log(`Master is running on process...`, process.pid);
-//     for (let i = 0; i < totalCpus; i++) {
-//         cluster.fork();
-//     }
-//     cluster.on("exit", (worker, code, signal) => {
-//         console.log(`worker ${worker.process.pid} died`);
-//         cluster.fork();
-//     });
-// }
-// else {
-const app = express()
+if (cluster.isMaster) {
+    console.log(`Number of CPUs is ${totalCpus}`);
+    console.log(`Master is running on process...`, process.pid);
+    for (let i = 0; i < totalCpus; i++) {
+        cluster.fork();
+    }
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+}
+
+const app: Application = express()
 app.use(express.json())
 console.log(`Worker started on process...`, process.pid);
-mockJob.start()
+job.start()
 app.use('/', publicRoutes)
 app.use(errorHandler)
 const port = process.env.PORT || 3000
-app.listen(port, () => {
-    console.log(`Server started on port ${port} ${process.pid}`)
-});
-// }
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => {
+        console.log(`Server started on port ${port} ${process.pid}`)
+    });
+}
+
+export default app;
